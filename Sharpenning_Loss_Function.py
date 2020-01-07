b9358fd38fd26b6123d870af903cdbd67bdeb4db
Sina-Mohammadi <36511576+Sina-Mohammadi@users.noreply.github.com>
2020-01-08 01:06:45 +0330

    Update Sharpenning_Loss_Function.py

--- Sharpenning_Loss_Function.py
+++ Sharpenning_Loss_Function.py
@@ -1,11 +1,9 @@
 from keras import backend as K
 
-
 '''Implementation of the proposed Sharpening Loss function.
    The Sharpening Loss results in sharper foreground objects and less blurry predictions.
    Moreover, as shown in the paper, the Sharpening Loss outperforms the Cross-entropy loss
    by a significant margin in saliency detetction task'''
-
 
 def Sharpenning_Loss(Lambda):
 
